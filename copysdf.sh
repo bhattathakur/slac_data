6964454b85916fbae26fa8e66fbc5e3080051b18
bhattathakur <bhattathaur2015@gmail.com>
2023-03-16 23:21:30 -0400

    updated /home/thakur/slac_data on Thu Mar 16 11:21:30 PM EDT 2023

--- copysdf.sh
+++ copysdf.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-dir_name=PZ1M
+dir_name=PX5M
 mkdir ${dir_name}
 
 scratch_path=/scratch/t/tbhatta
-destination=${scratch_path}/${dir_name}/merged/
+#destination=${scratch_path}/${dir_name}/merged/
+destination=${scratch_path}/${dir_name}/recon/
 
 scp tbhatta@sdf-login.slac.stanford.edu:${destination}*.root ./${dir_name}/
 
